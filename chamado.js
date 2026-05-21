96c498f967a0fb4449ca1f7c58fdfcd3db320877
Arthur Leandro <164565551+1537979@users.noreply.github.com>
2026-05-21 17:51:17 -0300

    Add files via upload

--- chamado.js
+++ chamado.js
@@ -2,14 +2,14 @@ async function enviar() {
       const data = {
         nome: document.getElementById("nome").value,
         email: document.getElementById("email").value,
-        tipo: "Teste",
-        prioridade: "Alta",
-        operadora: "Operadora X",
-        emailOperadora: "teste@email.com",
-        assunto: "Teste",
+        tipo: document.getElementById("tipo").value,
+        prioridade: document.getElementById("prioridade").value,
+        operadora: document.getElementById("operadora").value,
+        emailOperadora: "arthur.alj13@gmail.com",
+        assunto: "GETRAK / NEXCORP SERVIÇOS E TELECOMUNICAÇÕES S/A CNPJ: 06.349.959/0001-13 - " + document.getElementById("assunto").value,
         anexo: "",
-        descricao: "Teste de envio",
-        copia: ""
+        descricao: document.getElementById("descricao").value,
+        copia: document.getElementById("copia").value
       };
 
       const response = await fetch("https://script.google.com/macros/s/AKfycbxdlmjDbr4E_aSGwUprJyhB0mf-ifH0r3Vk9dgRpF1HUwsj7eMokkXGupftKfORujNqlA/exec", {
